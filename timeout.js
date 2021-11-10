8e8eac357aaced23c2c5a0d1744213671aafd1df
fred colin <fred.colind@gmail.com>
2021-11-10 14:15:34 +0100

    trying git action test result

---
+++ timeout.js
@@ -0,0 +1,21 @@
+function timeout(promise, time) {
+
+    return new Promise((resolve, reject) => {
+        promise.then(resolve()).catch(reject("error"));
+        setTimeout(() => {
+            reject("timeout");
+        }, time);
+    })
+}
+ 
+timeout(testPromise(500),1000).then(() => console.log("Test 1 Passed"), () => console.log("Test 1 Failed"))
+timeout(testPromise(1000), 500).then(() => console.log("Test 2 Failed"), () => console.log("Test 2 Passed"))
+ 
+ 
+function testPromise(time) {
+    return new Promise(resolve => setTimeout(resolve, time))
+}
+
+function hello() {
+    console.log("hello");
+}
